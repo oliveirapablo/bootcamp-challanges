8bd663ff58614f7eea932674595d9c8fffe0a25b
Pablo de Oliveira <pablo.d.o.s96@gmail.com>
2020-04-17 13:49:56 -0300

    refactor

--- challange-01/src/app.js
+++ challange-01/src/app.js
@@ -11,43 +11,21 @@ app.use(cors());
 
 const repositories = [];
 
-function loanMethod(request, response, next){
-  const { method, url } = request;
-
-  const loglabel = `${method.toUpperCase()}`;
-
-  switch(loglabel){
-    case 'GET':
-      console.log('should be able to list the repositories');
-      break;
-    case 'POST': 
-      console.log('should be able to create a new repository');
-      break;
-    case 'DELETE':
-      console.log('should be able to delete the repository');
-      break;
-    case 'PUT':
-      console.log('should not be able to update repository likes manually');
-  }
-
-  return next();
-}
-
-app.get("/repositories", loanMethod, (request, response) => {
+app.get("/repositories", (request, response) => {
   return response.json(repositories);
 });
 
-app.post("/repositories", loanMethod, (request, response) => {
+app.post("/repositories", (request, response) => {
   const { title, url, techs } = request.body;
 
-  const respositorie = { id: uuid(), title, url, techs };
+  const respositorie = { id: uuid(), title, url, techs, likes:0 };
 
   repositories.push(respositorie);
 
   return response.json(respositorie);
 });
 
-app.put("/repositories/:id", loanMethod, (request, response) => {
+app.put("/repositories/:id", (request, response) => {
   const{ id } = request.params;
   const { title, url, techs } = request.body;
 
@@ -62,27 +40,27 @@ app.put("/repositories/:id", loanMethod, (request, response) => {
     title,
     url,
     techs,
+    likes: repositories[respositorieIndex].likes,
   };
 
   repositories[respositorieIndex] = repositorie;
   return response.json(repositorie);
 });
 
-app.delete("/repositories/:id", loanMethod, (request, response) => {
+app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
   const respositorieIndex = repositories.findIndex(repositorie => repositorie.id ===id);
 
   if(respositorieIndex < 0){
     return response.status(400).json({error: 'Repositorie not found'})
-  }
-
+  }else{
   repositories.splice(respositorieIndex, 1);
-
+}
   return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", loanMethod, (request, response) => {
+app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
 
   const respositorieIndex = repositories.findIndex(repositorie => repositorie.id ===id);
@@ -91,8 +69,10 @@ app.post("/repositories/:id/like", loanMethod, (request, response) => {
     return response.status(400).json({error: 'Repositorie not found'})
   }
 
-   console.count(`Like`);
-   return response.status(200).send();
+  repositories[respositorieIndex].likes++;
+
+   
+   return response.json(repositories[respositorieIndex]);
 });
 
 module.exports = app;
